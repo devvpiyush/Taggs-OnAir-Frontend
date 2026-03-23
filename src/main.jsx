b7ef44ae4b2625c96c2b58580b62a58ae719367a
Devv Piyush <149592179+devvpiyush@users.noreply.github.com>
2026-03-23 12:47:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 // External Modules
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -47,9 +47,9 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  {/* <StrictMode> */}
     <QueryClientProvider client={new QueryClient()}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  </StrictMode>,
+  {/* </StrictMode>, */}
 );
