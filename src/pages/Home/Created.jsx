e7e7d48ff66d44fbaaa7b9f0d54a18d7bd70b1f8
Devv Piyush <149592179+devvpiyush@users.noreply.github.com>
2026-04-02 11:33:27 +0530

    Update Created.jsx

--- src/pages/Home/Created.jsx
+++ src/pages/Home/Created.jsx
@@ -3,7 +3,7 @@ import Check from "@icon/Check.svg";
 
 function Created() {
   return (
-    <div className="w-full px-4 pt-2">
+    <div className="w-full px-4 pt-4">
       <div className="w-full p-3 flex flex-row items-center justify-start gap-2 bg-[#16211C] border-2 border-green-900 rounded-xl">
         <img src={Check} alt="Check_Icon" />
         <h1
