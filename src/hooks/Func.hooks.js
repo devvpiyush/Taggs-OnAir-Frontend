5245f4d5c0ecf9be138b798d200773503355e511
Devv Piyush <149592179+devvpiyush@users.noreply.github.com>
2026-04-02 10:30:21 +0530

    Update Func.hooks.js

--- src/hooks/Func.hooks.js
+++ src/hooks/Func.hooks.js
@@ -5,6 +5,7 @@ import api from "@util/api.util";
 import { useQuery } from "@tanstack/react-query";
 
 export const useSearch = (input) => {
+if (input === "") return;
   input = input.trim().toLowerCase() || "";
 
   const call = async () => {
